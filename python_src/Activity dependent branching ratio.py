c46a47dd109b5d6d65d69937fa54fab13ea867fa
Abolfazl Ahmadi rahmat <38430894+ahmadiphy@users.noreply.github.com>
2019-01-10 00:07:42 +0330

    Add files via upload

---
+++ python_src/Activity dependent branching ratio.py
@@ -0,0 +1,66 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+import random
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
+# In[9]:
+
+
+def b(M,dl):
+    myl=[]
+    N=len(dl)
+    result=0
+    for i in range(N):
+        if dl[i]==M and i!=N-1:
+            myl.append(dl[i+1])
+    if len(myl)!=0:
+        c=Counter(myl)
+        xs=list(set(myl))
+        for i in xs:
+            result+=i*c[i]/M
+    return result/N
+def b_list(mydata):
+    m_list=list(set(mydata))
+    resultx=[]
+    resulty=[]
+    for i in m_list:
+        bm=b(i,mydata)
+        if bm!=0:
+            resultx.append(i)
+            resulty.append(bm)
+    return [resultx,resulty]
+
+
+# In[24]:
+
+
+random_list=[]
+for x in range(100000):
+  random_list.append(random.randint(1,1000))
+
+
+# In[25]:
+
+
+bm=b_list(random_list)
+
+
+# In[27]:
+
+
+p1,=plt.plot(bm[0],bm[1],linestyle='',marker='s')
+plt.show()
+
+
+# In[28]:
+
+
+p2,=plt.loglog(bm[0],bm[1],linestyle='',marker='s')
+plt.show()
+
