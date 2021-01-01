2f99e83c32a56690e09a34378f5fb3d7591d1613
Abolfazl Ahmadi-Rahmat <38430894+ahmadiphy@users.noreply.github.com>
2021-01-01 12:29:06 +0330

    Update fread.cpp

--- data_binning/fread.cpp
+++ data_binning/fread.cpp
@@ -9,13 +9,13 @@ Fread::Fread()
 void Fread::Read(ostringstream &fnm, iMatrix &mym)
 {
     string line;
-    ifstream data(fnm.str().c_str(),ios_base::binary);
+    ifstream data(fnm.str().c_str(), ios_base::binary);
     if (data.is_open())
     {
-        while(getline (data,line))
+        while(getline (data, line))
         {
             string::size_type sz;
-            double num = stod (line,&sz);
+            double num = stod (line, &sz);
             double value = stod (line.substr(sz));
             mym[0].push_back(num);
             mym[1].push_back(value);
